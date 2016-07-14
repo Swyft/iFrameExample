ebc29448d4113f09f0864f36bf8f0d73e62c1e98
David Fekke <david@fekke.com>
2016-07-14 09:11:55 -0400

    First commit

--- iFrameExample/ViewController.swift
+++ iFrameExample/ViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myUIWebView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var urlStr: NSString = "https://mobilecentral.swyfthub.com/SWY/MobileSiteV1/FrontDashboard.aspx"
+        urlStr = urlStr.appending("?clientcode=SWY")
+        urlStr = urlStr.appending("&userid=swy.mfitzpatrick&deviceid=31e8689a-f2fa-9b30-0f2d-486eabe1a234")
+        urlStr = urlStr.appending("&channel=1")
+        urlStr = urlStr.appending("&intertype=1")
+        urlStr = urlStr.appending("&location=1")
+        urlStr = urlStr.appending("&currlat=0")
+        urlStr = urlStr.appending("&currlng=0")
+        urlStr = urlStr.appending("&appversion=2.5.14")
+        urlStr = urlStr.appending("&platform=Desktop")
+        
+        let url: NSURL = NSURL(string: urlStr as String)!
+        let urlRequest = NSURLRequest(url: url as URL)
+        
+        myUIWebView.loadRequest(urlRequest as URLRequest)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func shouldAutorotateToInterfaceOrientation(interfaceOrientation: UIInterfaceOrientation) -> Bool {
+        return true
     }
 
 
